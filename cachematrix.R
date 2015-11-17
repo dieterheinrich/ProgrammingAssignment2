eb8e83f30ef36901df9f44cf081d2c17153cab2e
Dieter Heinrich <dieter.heinrich@uibk.ac.at>
2015-11-17 22:04:53 +0100

    Completed code of makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    matrix_inv <- NULL
+    ## define set function to set the values of a martix whos inverse is calculated
+    set <- function(y) {
+        x <- y
+        matrix_inv <- NULL
+    }
+    ## define get function to get the state/values of the current matrix
+    get <- function() x
+    ## define set_matrix_inv function to set the inverse of a matrix to a given value
+    set_matrix_inv <- function(minv) matrix_inv <- minv
+    ## define get_matrix_function to get the current state/valuses of the inverse matrix
+    get_matrix_inv <- function() matrix_inv
+    ## collect functions defined above in a list
+    list(set = set, get = get, 
+         set_matrix_inv = set_matrix_inv, 
+         get_matrix_inv = get_matrix_inv)
 }
 
 
